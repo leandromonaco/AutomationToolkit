6c92b9b458ef8a4d989b39d6854945d5d0fb35f0
leandromonaco <leandro.monaco@gmail.com>
2020-10-30 15:10:10 +1300

    SonaType - Removed unused class

--- src/AutomationToolkit.SonaType/Model/SonaTypeComponentScanResult.cs
+++ src/AutomationToolkit.SonaType/Model/SonaTypeComponentScanResult.cs
@@ -1,10 +1,12 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace AutomationToolkit.SonaType.Model
 {
-    public class SonaTypeComponentScanResults
+    public class SonaTypeComponentScanResult
     {
-        public List<SonaTypeComponentScanResult> ComponentScanResults { get; set; }
+        public string Coordinates { get; set; }
+        public string Description { get; set; }
+        public string Reference { get; set; }
+        public List<SonaTypeVulnerability> Vulnerabilities { get; set; }
     }
 }
