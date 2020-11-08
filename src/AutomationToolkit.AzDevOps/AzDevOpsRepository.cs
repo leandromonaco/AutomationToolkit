886e9dacf7c868e37863ac542bd26ab6e2948c48
leandromonaco <leandro.monaco@gmail.com>
2020-11-09 08:57:46 +1300

    Added new AzDevOps operations

--- src/AutomationToolkit.AzDevOps/AzDevOpsRepository.cs
+++ src/AutomationToolkit.AzDevOps/AzDevOpsRepository.cs
@@ -1,9 +1,15 @@
-﻿using AutomationToolkit.AzDevOps.Model.Repository;
+﻿using AutomationToolkit.AzDevOps.Model.Branch;
+using AutomationToolkit.AzDevOps.Model.Commit;
+using AutomationToolkit.AzDevOps.Model.PullRequest;
+using AutomationToolkit.AzDevOps.Model.Repository;
 using AutomationToolkit.Common.Http;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace AutomationToolkit.AzDevOps
 {
@@ -25,5 +31,48 @@ namespace AutomationToolkit.AzDevOps
             var azureDevOpsRepositories = JsonConvert.DeserializeObject<AzDevOpsCodeRepositories>(response);
             return azureDevOpsRepositories.Value.FirstOrDefault(r => r.Name.Equals(repositoryName));
         }
+
+        public async Task<List<AzDevOpsBranch>> GetBranchesAsync(string repositoryId)
+        {
+            var response = await _httpService.GetAsync($"{_baseUrl}/git/repositories/{repositoryId}/refs");
+            var azureDevOpsBranches = JsonConvert.DeserializeObject<AzDevOpsBranches>(response);
+            return azureDevOpsBranches.Value;
+        }
+
+        public async Task<List<AzDevOpsCommit>> GetCommitsAsync(string repositoryId, string branchName, DateTime from, DateTime to)
+        {
+            //Get Commits in the last month
+            var startDateStr = from.ToString("MM/dd/yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture);
+            var finishDateStr = to.ToString("MM/dd/yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture);
+
+            var response = await _httpService.GetAsync($"{_baseUrl}/git/repositories/{repositoryId}/commits?searchCriteria.itemVersion.version={HttpUtility.UrlEncode(branchName)}&searchCriteria.toDate={finishDateStr}&searchCriteria.fromDate={startDateStr}&searchCriteria.includePushData=true&$top=50");
+            var azureDevOpsCommits = JsonConvert.DeserializeObject<AzDevOpsCommits>(response);
+            return azureDevOpsCommits.Value;
+        }
+
+        public async Task<List<AzDevOpsPullRequest>> GetPullRequestsAsync(string repositoryId, bool isPullRequestCompleted)
+        {
+            string response;
+
+            if (isPullRequestCompleted)
+            {
+                response = await _httpService.GetAsync($"{_baseUrl}/git/repositories/{repositoryId}/pullrequests?searchCriteria.status=completed");
+            }
+            else
+            {
+                response = await _httpService.GetAsync($"{_baseUrl}/git/repositories/{repositoryId}/pullrequests");
+            }
+
+            return JsonConvert.DeserializeObject<AzDevOpsPullRequests>(response).Value;
+        }
+
+        public async Task<List<AzDevOpsPullRequestThread>> GetPullRequestThreadsAsync(string repositoryId, string pullRequestId)
+        {
+            var response = await _httpService.GetAsync($"{_baseUrl}/git/repositories/{repositoryId}/pullrequests/{pullRequestId}/threads");
+            var pullRequestThreads = JsonConvert.DeserializeObject<AzDevOpsPullRequestThreads>(response);
+            return pullRequestThreads.Value;
+        }
+
+
     }
 }
--- test/AutomationToolkit.Test/AzDevOpsTest.cs
+++ test/AutomationToolkit.Test/AzDevOpsTest.cs
@@ -1,6 +1,7 @@
 ﻿using AutomationToolkit.AzDevOps;
 using AutomationToolkit.SonaType;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -30,6 +31,12 @@ namespace AutomationToolkit.Test
         async Task GetRepository()
         {
             var repository = await _azDevOpsTestRepository.GetRepositoryAsync("Misc");
+            var branches = await _azDevOpsTestRepository.GetBranchesAsync(repository.Id);
+            var commits = await _azDevOpsTestRepository.GetCommitsAsync(repository.Id, "master", new DateTime(2020,1,1), new DateTime(2020, 12, 31));
+
+            var pendingPRs = await _azDevOpsTestRepository.GetPullRequestsAsync(repository.Id, false);
+            var completePRs = await _azDevOpsTestRepository.GetPullRequestsAsync(repository.Id, true);
+            var prThreads = await _azDevOpsTestRepository.GetPullRequestThreadsAsync(repository.Id, completePRs[0].PullRequestId);
         }
     }
 }
