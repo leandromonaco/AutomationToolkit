4aa0e6bf85dd4a00e5cc9391d4817a3e5bd78165
leandromonaco <leandro.monaco@gmail.com>
2020-11-23 09:29:03 +1300

    Added VersionOne library

---
+++ src/AutomationToolkit.VersionOne/Model/VersionOneIssue.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace AutomationToolkit.VersionOne.Model
+{
+    public class VersionOneIssue
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        [JsonProperty("Team.Name")]
+        public string Team { get; set; }
+        [JsonProperty("Category.Name")]
+        public string Category { get; set; }
+        public string IdentifiedBy { get; set; }
+        [JsonProperty("Owner.Name")]
+        public string Owner { get; set; }
+        public string Resolution { get; set; }
+        [JsonProperty("ResolutionReason.Name")]
+        public string ResolutionReason { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public VersionOneAssetState AssetState { get; set; }
+        [JsonProperty("_oid")]
+        public string Url { get; set; }
+        [JsonProperty("CreatedBy.Email")]
+        public string CreatedByEmail { get; set; }
+    }
+}
---
+++ src/AutomationToolkit.VersionOne/Model/VersionOneWorkItem.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace AutomationToolkit.VersionOne.Model
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class VersionOneWorkItem
+    {
+
+        [JsonProperty("Super.Number")]
+        public string SuperNumber { get; set; }
+        [JsonProperty("Super.Name")]
+        public string SuperName { get; set; }
+        [JsonProperty("Super.Order")]
+        public long SuperOrder { get; set; }
+        [JsonProperty("Super.Team.Name")]
+        public string SuperTeamName { get; set; }
+        [JsonProperty("Super.Scope.Name")]
+        public string SuperScopeName { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public double? Estimate { get; set; }
+        [JsonProperty("Status.Name")]
+        public string Status { get; set; }
+        [JsonProperty("Team.Name")]
+        public string Team { get; set; }
+        [JsonProperty("Timebox.Name")]
+        public string Sprint { get; set; }
+        public long Order { get; set; }
+        [JsonProperty("AssetState")]
+        public VersionOneAssetState State { get; set; }
+        [JsonProperty("ResolutionReason.Name")]
+        public object Custom1 { get; set; }
+        [JsonProperty("Custom_Severity.Name")]
+        public object Custom2 { get; set; }
+        [JsonProperty("_oid")]
+        public string Url { get; set; }
+        public DateTime ChangeDate { get; set; }
+        [JsonProperty("CreatedBy.Email")]
+        public string CreatedByEmail { get; set; }
+        [JsonProperty("Super.Priority.Name")]
+        public string SuperPriorityName { get; set; }
+    }
+}
